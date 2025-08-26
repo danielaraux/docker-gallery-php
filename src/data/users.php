47f0fc1dc708e6c723a462fbdf2811fa4aad7fd7
daniel <danielaraux@hotmail.com>
2025-08-26 10:26:16 +0200

    first commit

---
+++ src/data/users.php
@@ -0,0 +1,143 @@
+<?php
+$users = [
+    [
+        "id" => 4821,
+        "nom" => "Dupont",
+        "prenom" => "Marie",
+        "sexe" => "Femme",
+        "photo" => "photo_01.jpg"
+    ],
+    [
+        "id" => 7352,
+        "nom" => "Martin",
+        "prenom" => "Claire",
+        "sexe" => "Femme",
+        "photo" => "photo_02.jpg"
+    ],
+    [
+        "id" => 9183,
+        "nom" => "Bernard",
+        "prenom" => "Isabelle",
+        "sexe" => "Femme",
+        "photo" => "photo_03.jpg"
+    ],
+    [
+        "id" => 6049,
+        "nom" => "Durand",
+        "prenom" => "Sophie",
+        "sexe" => "Femme",
+        "photo" => "photo_04.jpg"
+    ],
+    [
+        "id" => 2765,
+        "nom" => "Petit",
+        "prenom" => "Camille",
+        "sexe" => "Femme",
+        "photo" => "photo_05.jpg"
+    ],
+    [
+        "id" => 8510,
+        "nom" => "Moreau",
+        "prenom" => "Julien",
+        "sexe" => "Homme",  
+        "photo" => "photo_06.jpg"
+    ],
+    [
+        "id" => 3298,
+        "nom" => "Fournier",
+        "prenom" => "Élodie",
+        "sexe" => "Femme",
+        "photo" => "photo_07.jpg"
+    ],
+    [
+        "id" => 4472,
+        "nom" => "Girard",
+        "prenom" => "Thomas",
+        "sexe" => "Homme",  
+        "photo" => "photo_08.jpg"
+    ],
+    [
+        "id" => 5106,
+        "nom" => "Bonnet",
+        "prenom" => "Antoine",
+        "sexe" => "Homme", 
+        "photo" => "photo_09.jpg"
+    ],
+    [
+        "id" => 6893,
+        "nom" => "Roux",
+        "prenom" => "Nicolas",
+        "sexe" => "Homme",  
+        "photo" => "photo_10.jpg"
+    ],
+    [
+        "id" => 7924,
+        "nom" => "Vincent",
+        "prenom" => "Laura",
+        "sexe" => "Femme",
+        "photo" => "photo_11.jpg"
+    ],
+    [
+        "id" => 1467,
+        "nom" => "Lefevre",
+        "prenom" => "Chloé",
+        "sexe" => "Femme",
+        "photo" => "photo_12.jpg"
+    ],
+    [
+        "id" => 8329,
+        "nom" => "Francois",
+        "prenom" => "Alexandre",
+        "sexe" => "Homme",  
+        "photo" => "photo_13.jpg"
+    ],
+    [
+        "id" => 2615,
+        "nom" => "Mercier",
+        "prenom" => "Émilie",
+        "sexe" => "Femme",
+        "photo" => "photo_14.jpg"
+    ],
+    [
+        "id" => 9750,
+        "nom" => "Blanc",
+        "prenom" => "Julie",
+        "sexe" => "Femme",
+        "photo" => "photo_15.jpg"
+    ],
+    [
+        "id" => 5384,
+        "nom" => "Garnier",
+        "prenom" => "Hugo",
+        "sexe" => "Homme",  
+        "photo" => "photo_16.jpg"
+    ],
+    [
+        "id" => 6012,
+        "nom" => "Chevalier",
+        "prenom" => "Adrien",
+        "sexe" => "Homme",  
+        "photo" => "photo_17.jpg"
+    ],
+    [
+        "id" => 8543,
+        "nom" => "Lopez",
+        "prenom" => "Anaïs",
+        "sexe" => "Femme",
+        "photo" => "photo_18.jpg"
+    ],
+    [
+        "id" => 3271,
+        "nom" => "Meyer",
+        "prenom" => "Paul",
+        "sexe" => "Homme",  
+        "photo" => "photo_19.jpg"
+    ],
+    [
+        "id" => 9098,
+        "nom" => "Schneider",
+        "prenom" => "Sarah",
+        "sexe" => "Femme",
+        "photo" => "photo_20.jpg"
+    ]
+];
---
+++ src/gallery.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Gallery Photos </title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body class="bg-light d-flex flex-column min-vh-100">
+
+    <h1 class="text-center py-4 bg-dark text-white">Custom Gallery</h1>
+
+    <main class="container py-4">
+
+    </main>
+
+    <footer class="bg-dark text-white text-center mt-auto py-4">
+        <p class="m-3">Afpa 2K25 - SuperGlobale - Session</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+
+</html>
